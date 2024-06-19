bd80ec9c5b44e7c33d0c7a1fa7e9be9d44ef56a8
Jhônatan Santana <codejhonatansantana@gmail.com>
2024-06-19 12:07:07 -0300

    feat: configuring retrofit service

--- app/src/main/java/br/com/appburgues/network/ServiceProvider.kt
+++ app/src/main/java/br/com/appburgues/network/ServiceProvider.kt
@@ -1,4 +1,23 @@
 package br.com.appburgues.network
 
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 class ServiceProvider {
+    private val baseUrl = ""
+
+    private val client = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+    fun<API> createService(apiClass: Class<API>): API = retrofit.create(apiClass)
 }
