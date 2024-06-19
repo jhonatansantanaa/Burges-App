357d3947653c6787a57137fd3d8d05e79f74fa6d
Jhônatan Santana <codejhonatansantana@gmail.com>
2024-06-19 11:21:56 -0300

    feat: structure project created

---
+++ app/src/main/java/br/com/appburgues/di/DomainModule.kt
@@ -0,0 +1,12 @@
+package br.com.appburgues.di
+
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DomainModule {
+
+}
---
+++ app/src/main/java/br/com/appburgues/network/ServiceProvider.kt
@@ -0,0 +1,4 @@
+package br.com.appburgues.network
+
+class ServiceProvider {
+}
